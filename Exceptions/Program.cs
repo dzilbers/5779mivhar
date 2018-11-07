f0ba5d5a777b503ee6cc10143beecaf77a6eee2c
Dan Zilberstein <dzilbers@gmail.com>
2018-11-07 09:59:11 +0200

    Exceptions

---
+++ Exceptions/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int i = int.Parse("abcd");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("It is not a number in the string :-(\n" + ex);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Why do you think I can decide on null?\n" + ex);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw new FormatException("Staaaam\n" + ex);
+            }
+        }
+    }
+}
---
+++ ExtensionFunctions/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Reflection;
+
+namespace ExtensionFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "123";
+            int i = str.ToInt();
+            Console.WriteLine(i);
+            //DateTime.Now.ToStringProperty();
+            new { Id = 123, Name = "Vazana" }.ToStringProperty();
+        }
+    }
+
+    static class MyTools
+    {
+        public static int ToInt(this string str)
+        {
+            return int.Parse(str);
+        }
+
+        /// <summary>
+        /// פונקציית ההרחבה האהובה עליי
+        /// </summary>
+        /// <typeparam name="T">Generic - any type :=)</typeparam>
+        /// <param name="t">this is the context object</param>
+        public static void ToStringProperty<T>(this T t)
+        {
+            string str = "";
+            foreach (var item in t.GetType().GetProperties())
+            {
+                str += "\n" + item.Name + ": " + item.GetValue(t, null);
+            }
+            Console.WriteLine(str);
+        }
+    }
+
+}
